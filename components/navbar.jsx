0216f722450d9676e9884727fa261d4fd81616d1
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2022-11-26 13:39:24 +0000

    sum41

---
+++ components/navbar.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export default class Navbar extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <nav></nav>
+        );
+    }
+}
+ 
